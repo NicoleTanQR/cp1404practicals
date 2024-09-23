9a0af36cfd134b78069e8b81912c7ae04db7bf6a
NicoleTanQR <absolutechaos78@gmail.com>
2024-09-23 15:17:48 +0800

    Change variable to constant

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -3,19 +3,20 @@ Practical 2 - Refactoring
 Password Check with Functions
 """
 
+MINIMUM_LENGTH = 7
+
 
 def main():
     """Print a line of asterisks based on password length."""
-    minimum_length = int(input("Password minimum length: "))
-    password = get_password("Password: ", minimum_length)
+    password = get_password("Password: ")
     print_star_line(password)
 
 
-def get_password(prompt, minimum_length):
+def get_password(prompt):
     """Get a valid password of at least minimum length."""
     password = input(prompt)
-    while len(password) < minimum_length:
-        print(f"Password needs to be at least {minimum_length} characters long")
+    while len(password) < MINIMUM_LENGTH:
+        print(f"Password needs to be at least {MINIMUM_LENGTH} characters long")
         password = input(prompt)
     return password
 
