905828ffdb89ba9e50c74b1300d37e6b26ddbcf8
NicoleTanQR <absolutechaos78@gmail.com>
2024-09-29 18:20:34 +0800

    Update file to address requirements

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -2,17 +2,23 @@
 CP1404/CP5632 - Practical
 Answer the following questions:
 1. When will a ValueError occur?
+When argument value is inappropriate like a string not representing
+an integer being given to int()
 2. When will a ZeroDivisionError occur?
+When second argument to division or modulo operation is zero like
+denominator for division in this case
 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+Yes
 """
 
 try:
     numerator = int(input("Enter the numerator: "))
     denominator = int(input("Enter the denominator: "))
+    while denominator == 0:
+        print("Denominator must not be zero!")
+        denominator = int(input("Enter the denominator: "))
     fraction = numerator / denominator
     print(fraction)
 except ValueError:
     print("Numerator and denominator must be valid numbers!")
-except ZeroDivisionError:
-    print("Cannot divide by zero!")
 print("Finished.")
