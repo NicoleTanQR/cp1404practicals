d70c49194c09b775597524aab8543b9b06bd5d7c
NicoleTanQR <absolutechaos78@gmail.com>
2024-09-29 12:44:16 +0800

    Edit line length as per PEP 8

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -10,8 +10,10 @@
 # Could not have produced a 4
 
 # For print(random.uniform(2.5, 5.5))
-# Saw 3.5888765550500317, 3.525665531414343, and 4.808999839365551 on the console
-# Smallest I could have seen is 2.5, largest 5.5 or a number less than and closest to 5.5 depending on rounding
+# Saw 3.5888765550500317, 3.525665531414343, and 4.808999839365551 on
+# the console
+# Smallest I could have seen is 2.5, largest 5.5 or a number less than
+# and closest to 5.5 depending on rounding
 
 from random import randint
 
