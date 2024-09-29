adb31f28a41f828c362b6fee4b1339adad1e8a20
NicoleTanQR <absolutechaos78@gmail.com>
2024-09-29 15:51:33 +0800

    Update program to write output to file

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -14,10 +14,11 @@ MAX_DECREASE = 0.05  # 5%
 MIN_PRICE = 1.0
 MAX_PRICE = 100.0
 INITIAL_PRICE = 10.0
+FILENAME = "stock_price.txt"
 number_of_days = 0
 price = INITIAL_PRICE
-print(f"Starting price: ${price:,.2f}")
-
+out_file = open(FILENAME, 'w')
+print(f"Starting price: ${price:,.2f}", file=out_file)
 while MIN_PRICE <= price <= MAX_PRICE:
     price_change = 0
     # generate a random integer of 1 or 2
@@ -33,4 +34,5 @@ while MIN_PRICE <= price <= MAX_PRICE:
 
     number_of_days += 1
     price *= (1 + price_change)
-    print(f"On day {number_of_days} price is: ${price:,.2f}")
+    print(f"On day {number_of_days} price is: ${price:,.2f}", file=out_file)
+out_file.close()
