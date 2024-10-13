819eefd774f965ca8ca27cf9f9cf5db04b1ef754
NicoleTanQR <absolutechaos78@gmail.com>
2024-10-13 18:15:06 +0800

    Complete word occurrences exercise

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,15 +1,16 @@
 """
 Word Occurrences
 Estimate: 40 minutes
-Actual:    minutes
+Actual:   98 minutes
 """
 
-dictionary = {}
-text = input("Text: ").split()
-for word in text:
-    if word in dictionary:
-        dictionary[word] += 1
+word_to_count = {}
+words = input("Text: ").split()
+for word in words:
+    if word in word_to_count:
+        word_to_count[word] += 1
     else:
-        dictionary[word] = 1
-for word, count in dictionary.items():
-    print(f"{word} : {count}")
+        word_to_count[word] = 1
+word_width = max((len(word) for word in word_to_count))
+for word, count in sorted(word_to_count.items()):
+    print(f"{word:{word_width}} : {count}")
