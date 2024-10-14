1b9a404b6d4380feaf20a4677ab180c469bf0153
NicoleTanQR <absolutechaos78@gmail.com>
2024-10-14 12:12:36 +0800

    Change for loop approach from LBYL to EAFP

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -7,9 +7,9 @@ Actual:   98 minutes
 word_to_count = {}
 words = input("Text: ").split()
 for word in words:
-    if word in word_to_count:
+    try:
         word_to_count[word] += 1
-    else:
+    except KeyError:
         word_to_count[word] = 1
 word_width = max((len(word) for word in word_to_count))
 for word, count in sorted(word_to_count.items()):
