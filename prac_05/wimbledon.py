f55bb2579de64a1260f57965f3ba33840dc3e171
NicoleTanQR <absolutechaos78@gmail.com>
2024-10-18 18:38:57 +0800

    Complete game, set, match exercise

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,23 +1,49 @@
 """
 Game, Set, Match
 Estimate: 70 minutes
-Actual:    minutes
+Actual:  148 minutes
 """
 
 FILENAME = "wimbledon.csv"
-champion_to_number_of_wins = {}
-countries = set()
-print("Wimbledon Champions: ")
-with open(FILENAME, encoding="utf-8-sig") as in_file:
-    records = in_file.readlines()[1:]
-for record in records:
-    record = record.split(",")
-    champion, country = record[2], record[1]
-    champion_to_number_of_wins[champion] = champion_to_number_of_wins.get(
-        champion, 0) + 1
-    countries.add(country)
-for champion, number_of_wins in champion_to_number_of_wins.items():
-    print(f"{champion} {number_of_wins}")
-print()
-print("These 12 countries have won Wimbledon: ")
-print(", ".join(sorted(countries)))
+INDEX_COUNTRY = 1
+INDEX_CHAMPION = 2
+
+
+def main():
+    """Display champions and their win count and countries from file."""
+    records = load_records(FILENAME)
+    champion_to_number_of_wins, countries = process_records(records)
+    display_champion_details(champion_to_number_of_wins, countries)
+
+
+def load_records(filename):
+    """Load records from file."""
+    with open(filename, encoding="utf-8-sig") as in_file:
+        records = in_file.readlines()[1:]
+    return records
+
+
+def process_records(records):
+    """Return dictionary of champions and set of countries from records."""
+    champion_to_number_of_wins = {}
+    countries = set()
+    for record in records:
+        record = record.split(",")
+        champion, country = record[INDEX_CHAMPION], record[INDEX_COUNTRY]
+        champion_to_number_of_wins[champion] = champion_to_number_of_wins.get(
+            champion, 0) + 1
+        countries.add(country)
+    return champion_to_number_of_wins, countries
+
+
+def display_champion_details(champion_to_number_of_wins, countries):
+    """Display champion details stored."""
+    print("Wimbledon Champions: ")
+    for champion, number_of_wins in champion_to_number_of_wins.items():
+        print(champion, number_of_wins)
+    print()
+    print(f"These {len(countries)} countries have won Wimbledon: ")
+    print(", ".join(sorted(countries)))
+
+
+main()
