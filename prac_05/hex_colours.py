a72059156cacaa8820009bcd79afd13e94ced14a
NicoleTanQR <absolutechaos78@gmail.com>
2024-10-18 15:49:08 +0800

    Update program with get method

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -7,13 +7,20 @@ NAME_TO_CODE = {"absolute zero": "#0048ba", "aliceblue": "#f0f8ff",
                 "rifle green": "#444c38", "rocket metallic": "#8a7f80"}
 print(NAME_TO_CODE)
 
-is_valid_input = False
-while not is_valid_input:
-    try:
-        colour_name = input("Enter colour name: ").lower()
-        if colour_name != "":
-            print(NAME_TO_CODE[colour_name])
-        else:
-            is_valid_input = True
-    except KeyError:
-        print("Colour name not found")
+# Use EAFP approach
+# is_valid_input = False
+# while not is_valid_input:
+#     try:
+#         colour_name = input("Enter colour name: ").lower()
+#         if colour_name != "":
+#             print(NAME_TO_CODE[colour_name])
+#         else:
+#             is_valid_input = True
+#     except KeyError:
+#         print("Colour name not found")
+
+# Use get method
+colour_name = input("Enter a colour name: ").lower()
+while colour_name != "":
+    print(NAME_TO_CODE.get(colour_name))
+    colour_name = input("Enter a colour name: ").lower()
