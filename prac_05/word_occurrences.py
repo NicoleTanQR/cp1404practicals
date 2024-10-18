4836a53e014461b7c784c3dd8724bdd843008723
NicoleTanQR <absolutechaos78@gmail.com>
2024-10-18 16:07:05 +0800

    Change for loop approach from EAFP to using get method

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -7,10 +7,7 @@ Actual:   98 minutes
 word_to_count = {}
 words = input("Text: ").split()
 for word in words:
-    try:
-        word_to_count[word] += 1
-    except KeyError:
-        word_to_count[word] = 1
+    word_to_count[word] = word_to_count.get(word, 0) + 1
 word_width = max((len(word) for word in word_to_count))
 for word, count in sorted(word_to_count.items()):
     print(f"{word:{word_width}} : {count}")
