8b9969174069d671976ea9bb49ed77e037b8bbcf
NicoleTanQR <absolutechaos78@gmail.com>
2024-11-01 00:41:14 +0800

    Update file with ProgrammingLanguage class

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,6 +1,20 @@
 """
 Intermediate Exercises - Class
 Estimated work duration: 30 min
-Current time: a.m.
-Actual work duration:
+Current time: 12:26 a.m.
+Actual work duration: 12 min before Client code
 """
+
+
+class ProgrammingLanguage:
+    """Represent a ProgrammingLanguage object."""
+
+    def __init__(self, typing="Dynamic", reflection=False, year=0):
+        """Initialise a ProgrammingLanguage instance."""
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        """Return True or False based on typing."""
+        return self.typing == "Dynamic"
