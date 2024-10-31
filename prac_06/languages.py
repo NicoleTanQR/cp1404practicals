5dd0f917fdd1492a6dfb7ebb1abb4c9db1305ca0
NicoleTanQR <absolutechaos78@gmail.com>
2024-11-01 00:24:23 +0800

    Add intermediate exercises files

---
+++ prac_06/languages.py
@@ -0,0 +1,6 @@
+"""
+Intermediate Exercises - Client code
+Estimated work duration: 30 min
+Current time: a.m.
+Actual work duration:
+"""
---
+++ prac_06/programming_language.py
@@ -0,0 +1,6 @@
+"""
+Intermediate Exercises - Class
+Estimated work duration: 30 min
+Current time: a.m.
+Actual work duration:
+"""
