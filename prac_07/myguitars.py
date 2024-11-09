6b6dd85a060d7f4da272002fc154644f15ef93e4
NicoleTanQR <absolutechaos78@gmail.com>
2024-11-09 22:45:25 +0800

    Complete first part of More Guitars exercise

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -1,1 +1,40 @@
 """More Guitars! - Program to use the class"""
+
+from prac_07.guitar import Guitar
+
+FILENAME = "guitars.csv"
+INDEX_NAME = 0
+INDEX_YEAR = 1
+INDEX_COST = 2
+
+
+def main():
+    """Read file of guitar details, save as objects, display."""
+    guitars = load_guitars(FILENAME)
+    display_guitar_details(guitars)
+
+
+def load_guitars(filename):
+    """Load guitars from filename."""
+    guitars = []
+    in_file = open(filename)
+    for line in in_file:
+        parts = line.strip().split(',')
+        name = parts[INDEX_NAME]
+        year = int(parts[INDEX_YEAR])
+        cost = float(parts[INDEX_COST])
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+    in_file.close()
+    return guitars
+
+
+def display_guitar_details(guitars):
+    """Display guitar details stored."""
+    guitars.sort()
+    print("These are my guitars:")
+    for guitar in guitars:
+        print(guitar)
+
+
+main()
