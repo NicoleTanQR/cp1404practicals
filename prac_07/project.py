a1b1c3313dacbe40ac0044f3b1c7b6821e24ccc6
NicoleTanQR <absolutechaos78@gmail.com>
2024-11-10 06:04:16 +0800

    Add project management program files

---
+++ prac_07/project.py
@@ -0,0 +1,4 @@
+"""
+Project Management Program - Class
+Estimated work duration for both class & client code: 3 hours
+"""
---
+++ prac_07/project_management.py
@@ -0,0 +1,4 @@
+"""
+Project Management Program - Client code
+Estimated work duration for both class & client code: 3 hours
+"""
