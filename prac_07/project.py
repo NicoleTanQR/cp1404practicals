515039c735de9ec050f15a4000b018478d75d9f1
NicoleTanQR <absolutechaos78@gmail.com>
2024-11-10 07:13:06 +0800

    Update files with structure

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,4 +1,15 @@
 """
 Project Management Program - Class
 Estimated work duration for both class & client code: 3 hours
+Current time: 6:31 a.m.
 """
+
+
+class Project:
+    """Represent a Project object."""
+
+    def __init__(self):
+        """Initialise a Project instance."""
+
+    def __str__(self):
+        """Display data for a project."""
--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -1,4 +1,48 @@
 """
 Project Management Program - Client code
 Estimated work duration for both class & client code: 3 hours
+Current time: 6:31 a.m.
 """
+
+import datetime
+
+from prac_07.project import Project
+
+MENU = """- (L)oad projects  
+- (S)ave projects  
+- (D)isplay projects  
+- (F)ilter projects by date
+- (A)dd new project  
+- (U)pdate project
+- (Q)uit
+"""
+
+def main():
+    """Load and save project details, use project objects."""
+    filename = "projects.txt"
+    projects = load_projects(filename)
+    print(f"Welcome to Pythonic Project Management\nLoaded {len(projects)} "
+          f"projects from {filename}")
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "L":
+            pass
+        elif choice == "S":
+            pass
+        elif choice == "D":
+            pass
+        elif choice == "F":
+            pass
+        elif choice == "A":
+            pass
+        elif choice == "U":
+            pass
+        else:
+            print("Invalid menu choice")
+        print(MENU)
+        choice = input(">>> ").upper()
+    pass
+
+
+main()
