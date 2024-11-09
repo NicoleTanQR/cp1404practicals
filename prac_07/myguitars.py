8b07d7039b2ec2fa688cd18123412a074fdbb03f
NicoleTanQR <absolutechaos78@gmail.com>
2024-11-10 04:29:45 +0800

    Complete next part of More Guitars exercise

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -12,6 +12,8 @@ def main():
     """Read file of guitar details, save as objects, display."""
     guitars = load_guitars(FILENAME)
     display_guitar_details(guitars)
+    get_new_guitars(guitars)
+    save_guitars(FILENAME, guitars)
 
 
 def load_guitars(filename):
@@ -35,6 +37,26 @@ def display_guitar_details(guitars):
     print("These are my guitars:")
     for guitar in guitars:
         print(guitar)
+
+
+def get_new_guitars(guitars):
+    """Get new guitars from user input."""
+    name = input("Any new guitars?\nName: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        print(f"{guitar} added.")
+        name = input("Name: ")
+
+
+def save_guitars(filename, guitars):
+    """Overwrite file contents with updated guitars."""
+    out_file = open(filename, 'w')
+    for guitar in guitars:
+        print(f"{guitar.name},{guitar.year},{guitar.cost}", file=out_file)
+    out_file.close()
 
 
 main()
