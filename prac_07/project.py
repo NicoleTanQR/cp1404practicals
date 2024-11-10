f635c08309ebd14fcc9c1e681cc129d2e9fdcea4
NicoleTanQR <absolutechaos78@gmail.com>
2024-11-10 11:12:27 +0800

    Update class to attain display string

--- prac_07/project.py
+++ prac_07/project.py
@@ -8,8 +8,15 @@ Current time: 6:31 a.m.
 class Project:
     """Represent a Project object."""
 
-    def __init__(self):
+    def __init__(self, name="", date="", priority=0, cost=0.0, completion=0):
         """Initialise a Project instance."""
+        self.name = name
+        self.date = date
+        self.priority = priority
+        self.cost = cost
+        self.completion = completion
 
     def __str__(self):
         """Display data for a project."""
+        return (f"{self.name}, start: {self.date}, priority {self.priority}, "
+                f"estimate: ${self.cost:,.2f}, completion: {self.completion}%")
