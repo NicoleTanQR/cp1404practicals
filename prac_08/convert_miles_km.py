d997610a703fbbf039b2b01ea57c5dd538b86697
NicoleTanQR <absolutechaos78@gmail.com>
2024-11-17 18:27:02 +0800

    Complete Miles to Kilometres Converter exercise

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -6,6 +6,7 @@ from kivy.properties import StringProperty
 
 ONE_MILE_IN_KILOMETRES = 1.60934
 
+
 class MilesConverterApp(App):
     """Convert miles to kilometres."""
     output_kilometres = StringProperty()
@@ -18,11 +19,20 @@ class MilesConverterApp(App):
 
     def handle_convert(self, text):
         """Handle pressing convert button."""
-        self.output_kilometres = str(float(text) * ONE_MILE_IN_KILOMETRES)
+        self.output_kilometres = str(
+            self.get_valid_number(text) * ONE_MILE_IN_KILOMETRES)
 
     def handle_increment(self, text, change):
         """Handle pressing up or down button."""
-        self.root.ids.input_miles.text = str(float(text) + change)
+        self.root.ids.input_miles.text = str(
+            self.get_valid_number(text) + change)
+
+    def get_valid_number(self, text):
+        """Get a valid number, return 0.0 for invalid number."""
+        try:
+            return float(text)
+        except ValueError:
+            return 0.0
 
 
 MilesConverterApp().run()
