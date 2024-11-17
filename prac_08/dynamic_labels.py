11dd850aab075b2e64f83e29f2c0900968110710
NicoleTanQR <absolutechaos78@gmail.com>
2024-11-17 21:43:16 +0800

    Add Dynamic Labels exercise files

---
+++ prac_08/dynamic_labels.py
@@ -0,0 +1,38 @@
+"""Dynamic Labels - Class"""
+
+from random import randint
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
+
+NAMES = ["Alex", "Taylor", "Jordan", "Casey", "Keiko"]
+MINIMUM_NUMBER = 0
+MAXIMUM_NUMBER = 100
+
+
+class DynamicLabelsApp(App):
+    """Dynamically create a separate label for each name."""
+
+    def __init__(self, **kwargs):
+        """Construct main app."""
+        super().__init__(**kwargs)
+        self.name_to_label = {}
+
+    def build(self):
+        """Build the Kivy GUI."""
+        self.title = "Dynamic Labels"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        self.create_labels()
+        return self.root
+
+    def create_labels(self):
+        """Create label for name, adding to dictionary and GUI."""
+        for name in NAMES:
+            temp_label = Label(
+                text=f"{name}_{randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)}")
+            self.name_to_label[name] = temp_label
+            self.root.ids.main.add_widget(temp_label)
+
+
+DynamicLabelsApp().run()
