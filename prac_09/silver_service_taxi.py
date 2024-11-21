eb80828dec30c66e5b59b13fe34ebcc0fc40fed1
NicoleTanQR <absolutechaos78@gmail.com>
2024-11-22 04:09:27 +0800

    Complete SilverServiceTaxi exercise

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -1,1 +1,22 @@
 """SilverServiceTaxi class"""
+
+from prac_09.taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+    """Represent a SilverServiceTaxi object."""
+    flagfall = 4.50
+
+    def __init__(self, name, fuel, fanciness: float):
+        """Initialise a SilverServiceTaxi instance, based on Taxi."""
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km *= fanciness
+
+    def __str__(self):
+        """Return a string like a Taxi but with the extra charge."""
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
+
+    def get_fare(self):
+        """Return the price for the taxi trip."""
+        return super().get_fare() + self.flagfall
--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -1,1 +1,18 @@
 """Test SilverServiceTaxi"""
+
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    """Test SilverServiceTaxi class."""
+    hummer = SilverServiceTaxi("Hummer", 200, 4)
+    print(hummer)
+    assert hummer.get_fare() == 4.50
+    my_silver_service_taxi = SilverServiceTaxi("My Silver Service Taxi", 200,
+                                               2)
+    my_silver_service_taxi.drive(18)
+    print(my_silver_service_taxi)
+    assert my_silver_service_taxi.get_fare() == 48.78
+
+
+main()
