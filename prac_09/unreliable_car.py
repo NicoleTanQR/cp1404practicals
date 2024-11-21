40abf9f8cc1adbc3f6bd9425ca5f1c1e37cd4a2d
NicoleTanQR <absolutechaos78@gmail.com>
2024-11-22 00:26:12 +0800

    Complete UnreliableCar exercise

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -1,1 +1,22 @@
 """UnreliableCar class"""
+
+from random import uniform
+
+from prac_09.car import Car
+
+MINIMUM_NUMBER = 0
+MAXIMUM_NUMBER = 100
+
+
+class UnreliableCar(Car):
+    """Specialised version of a Car with reliability aspect."""
+
+    def __init__(self, name, fuel, reliability: float):
+        """Initialise an UnreliableCar instance, based on Car class."""
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+    def drive(self, distance):
+        """Drive like Car if random number less than reliability."""
+        if uniform(MINIMUM_NUMBER, MAXIMUM_NUMBER) < self.reliability:
+            super().drive(distance)
