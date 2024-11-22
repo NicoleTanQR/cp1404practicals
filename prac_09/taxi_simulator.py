d53cc89c75b7e1386694aca7b4fd622f0c3ede4a
NicoleTanQR <absolutechaos78@gmail.com>
2024-11-23 05:29:52 +0800

    Add taxi simulator program structure

---
+++ prac_09/taxi_simulator.py
@@ -0,0 +1,29 @@
+"""Taxi Simulator"""
+
+from prac_09.taxi import Taxi
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+MENU = "q)uit, c)hoose taxi, d)rive"
+
+
+def main():
+    """Drive taxi based on input and display costs."""
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2),
+             SilverServiceTaxi("Hummer", 200, 4)]
+    print("Let's drive!")
+    print(MENU)
+    choice = input(">>> ").lower()
+    while choice != "q":
+        if choice == "c":
+            pass
+        elif choice == "d":
+            pass
+        else:
+            print("Invalid option")
+        pass
+        print(MENU)
+        choice = input(">>> ").lower()
+    pass
+
+
+main()
