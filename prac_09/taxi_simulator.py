71aa1a509e5178b2b2adbf557c8ce60326d9837f
NicoleTanQR <absolutechaos78@gmail.com>
2024-11-24 17:22:28 +0800

    Complete taxi simulator program

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -7,7 +7,7 @@ MENU = "q)uit, c)hoose taxi, d)rive"
 
 
 def main():
-    """Drive taxi based on input and display costs."""
+    """Let user choose and drive taxi, display costs."""
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2),
              SilverServiceTaxi("Hummer", 200, 4)]
     current_taxi = None
@@ -25,7 +25,9 @@ def main():
         print(f"Bill to date: ${total_cost:.2f}")
         print(MENU)
         choice = input(">>> ").lower()
-    pass
+    print(f"Total trip cost: ${total_cost:.2f}")
+    print("Taxis are now:")
+    display_taxis(taxis)
 
 
 def choose_taxi(taxis):
@@ -33,8 +35,11 @@ def choose_taxi(taxis):
     print("Taxis available:")
     display_taxis(taxis)
     taxi_number = int(input("Choose taxi: "))
-    taxi = taxis[taxi_number]
-    return taxi
+    try:
+        taxi = taxis[taxi_number]
+        return taxi
+    except IndexError:
+        print("Invalid taxi choice")
 
 
 def display_taxis(taxis):
@@ -45,11 +50,15 @@ def display_taxis(taxis):
 
 def handle_trip(taxi, total_cost):
     """Handle one taxi trip."""
-    distance = int(input("Drive how far? "))
-    taxi.drive(distance)
-    trip_cost = taxi.get_fare()
-    print(f"Your {taxi.name} trip will cost you ${trip_cost:.2f}")
-    total_cost += trip_cost
+    if taxi:
+        taxi.start_fare()
+        distance = float(input("Drive how far? "))
+        taxi.drive(distance)
+        trip_cost = taxi.get_fare()
+        print(f"Your {taxi.name} trip cost you ${trip_cost:.2f}")
+        total_cost += trip_cost
+    else:
+        print("You need to choose a taxi before you can drive")
     return total_cost
 
 
