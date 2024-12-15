828c99e968faf12ef70ec9272e5af12d4773fa7f
NicoleTanQR <absolutechaos78@gmail.com>
2024-12-15 21:33:04 +0800

    Complete Wikipedia API & Python Library exercise

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,1 +1,24 @@
 """Wikipedia API & Python Library"""
+
+import wikipedia
+
+
+def main():
+    """Get page title or search phrase, display page details."""
+
+    search_string = input("Enter page title: ")
+    while search_string != "":
+        try:
+            search_result = wikipedia.page(search_string, auto_suggest=False)
+            print(search_result.title, search_result.summary.strip(),
+                  search_result.url, sep='\n')
+        except wikipedia.exceptions.PageError:  # e.g., input is "zvv"
+            print(f'Page id "{search_string}" does not match any pages. '
+                  f'Try another id!')
+        except wikipedia.exceptions.DisambiguationError as e:
+            print(f"We need a more specific title. Try one of the following, "
+                  f"or a new search:\n{e.options[:5]}...")
+        search_string = input("Enter page title: ")
+
+
+main()
